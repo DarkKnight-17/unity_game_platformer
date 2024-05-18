302be09360625005400ecbe22c697c4078a18f76
Dark_Knight <rustem_zhumabek@mail.ru>
2024-05-19 01:03:32 +0500

    half of the work is done

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,16 +6,42 @@ public class Player : MonoBehaviour
 {
     // Start is called before the first frame update
 
-    private Rigidbody2D rigidbody;
+
     public float speed;
+
+    public float jump;
+    public LayerMask ground;
+    private Rigidbody2D rigidBody;
+    private Collider2D playerCollider;
+    private Animator animator;
+
+    
+    
     void Start()
     {
-        rigidbody = GetComponent<Rigidbody2D>();
+        rigidBody = GetComponent<Rigidbody2D>();
+        playerCollider = GetComponent<Collider2D>();    
+      animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        rigidbody.velocity = new Vector2(speed, rigidbody.velocity.y);
+        rigidBody.velocity = new Vector2(speed, rigidBody.velocity.y);
+
+        bool isGrounded = Physics2D.IsTouchingLayers(playerCollider, ground);
+
+
+    
+      if(Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.Space)) {
+
+            if (isGrounded)
+            {
+            rigidBody.velocity = new Vector2(rigidBody.velocity.x, jump);
+            }
+        }
+            animator.SetBool("Grounded", isGrounded);
+
+        
     }
 }
